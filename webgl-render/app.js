aa53e36f9305673f4394bd0485db12e52cd39f4f
Meo <meo.ekroos@gmail.com>
2020-03-04 20:57:57 +0200

    Add fragment and vertex shaders for initial test

--- webgl-render/app.js
+++ webgl-render/app.js
@@ -5,26 +5,63 @@ var OBJ = require('webgl-obj-loader');
 
 var meshPath = '../models/crystal.obj';
 var opt = { encoding: 'utf8' };
-var mesh;
 
 const readMeshes = () => {
     fs.readFile(meshPath, opt, function (err, data) {
         if (err) return console.error(err);
         mesh = new OBJ.Mesh(data);
+        return mesh;
     });
 };
 
-const webGLStart = (meshes) => { 
-    app.meshes = meshes;
-    // initialize the VBOs
-    OBJ.initMeshBuffers(gl, mesh);
+const webGLStart = (mesh) => { 
 
+    // make sure you have vertex, vertex normal, and texture coordinate
+    // attributes located in your shaders and attach them to the shader program
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    
+    shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+    gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+    
+    shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+    
     // refer to the initMeshBuffers docs for an example of
     // how to render the mesh to the screen after calling
     // initMeshBuffers
+    // initialize the VBOs
+    OBJ.initMeshBuffers(gl, mesh);
+
+    // now to render the mesh
+    gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    // it's possible that the mesh doesn't contain
+    // any texture coordinates (e.g. suzanne.obj in the development branch).
+    // in this case, the texture vertexAttribArray will need to be disabled
+    // before the call to drawElements
+    if(!mesh.textures.length) {
+        gl.disableVertexAttribArray(shaderProgram.textureCoordAttribute);
+    }
+    else {
+        // if the texture vertexAttribArray has been previously
+        // disabled, then it needs to be re-enabled
+        gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+        gl.bindBuffer(gl.ARRAY_BUFFER, mesh.textureBuffer);
+        gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mesh.textureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.mesh.indexBuffer);
+    gl.drawElements(gl.TRIANGLES, model.mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
 window.onload = () => {
-    readMeshes.then(webGLStart);
+    readMeshes.then( (mesh) => {
+        webGLStart(mesh)
+    });
 }
 
