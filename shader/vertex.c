a112c5d9201d0a547556c65b3e10c298da0a5840
Meo <meo.ekroos@gmail.com>
2020-03-06 22:29:13 +0200

    Add basic styling for site, switch from rendering tutorial triangle to trying to render cube.

--- shader/vertex.c
+++ shader/vertex.c
@@ -1,7 +1,18 @@
-attribute vec4 a_position;
+attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
+attribute vec2 aTextureCoord;
 
-uniform mat4 u_matrix;
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform mat3 uNMatrix;
 
-void main() {
-  gl_Position = u_matrix * a_position;
+varying vec2 vTextureCoord;
+varying vec3 vTransformedNormal;
+varying vec4 vPosition;
+
+void main(void) {
+    vPosition = uMVMatrix * vec4(aVertexPosition, 1.0);
+    gl_Position = uPMatrix * vPosition;
+    vTextureCoord = aTextureCoord;
+    vTransformedNormal = uNMatrix * aVertexNormal;
 }
